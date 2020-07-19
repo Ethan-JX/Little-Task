c193701a2fa42ba94c083e059223f8a27851e8ae
Ethan-JX <Ethan-JX@example.com>
2020-07-19 18:59:36 +0800

    Task-Admin add Spring dependence

---
+++ Little-Task-Admin/src/main/java/com/little/project/task/TaskAdminApplication.java
@@ -0,0 +1,11 @@
+package com.little.project.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskAdminApplication.class, args);
+    }
+}
